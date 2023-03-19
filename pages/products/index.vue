db28f309f8c07022fb1c58989f2e598dd08ec84b
beachislove <asperatuz@yandex.ru>
2023-03-19 13:30:29 +0600

    feat: add search filter

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,66 +1,41 @@
 <template>
   <div class="">
-    <div class="mb-10">
-      <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >Search</label
-      >
-      <div class="relative">
-        <div
-          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search Mockups, Logos..."
-          required
-        />
-        <!-- <button
-          type="submit"
-          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button> -->
-      </div>
-    </div>
-    <div class="grid grid-cols-4 gap-4">
+    <Filter />
+    <div v-if="!error" class="grid grid-cols-4 gap-4">
       <div v-for="product in products" :key="product">
         <ProductCard :product="product" />
+      </div>
+    </div>
+    <div v-else>
+      <div class="flex mx-auto p-11 shadow-sm self-center justify-center">
+        <span class="text-4xl flex">NOT FOUND</span>
       </div>
     </div>
   </div>
 </template>
 
-<script setup >
+<script setup>
 import { dataStore } from "@/stores/store";
 import { storeToRefs } from "pinia";
 
 const store = dataStore();
 
 const { fetchProducts } = store;
-const { products } = storeToRefs(store);
-
+const { products, error } = storeToRefs(store);
 await fetchProducts();
+// await getError();
+// console.log(setInterval(() => {
+//
+// }), 5000)
+setInterval(() => {
+  console.log(error.value);
+}, 1000);
 
-console.dir(products);
+
+// store.$subscribe((mutation, piniaState) => {
+//   // isLoading.value = piniaState.isLoading;
+//   // error.value = piniaState.error;
+// });
 
 useHead({
   title: "Merch page",
--- stores/store.js
+++ stores/store.js
@@ -7,21 +7,23 @@ export const dataStore = defineStore("products", {
     sortOption: "name",
     sortOrder: "asc",
     searchTerm: "",
+    error: false,
   }),
 
   getters: {
+    // getError() {
+    //   return this.error
+    // },
     // getProducts() {
     //   return this.products;
     // },
     // filteredProducts: (state) => {
     //   let filteredProducts = [...state.products];
-
     //   if (state.searchTerm) {
     //     filteredProducts = filteredProducts.filter((product) =>
     //       product.name.toLowerCase().includes(state.searchTerm.toLowerCase())
     //     );
     //   }
-
     //   if (state.sortOption) {
     //     const sortField = state.sortOption;
     //     const sortOrder = state.sortOrder === "asc" ? 1 : -1;
@@ -29,12 +31,14 @@ export const dataStore = defineStore("products", {
     //       a[sortField] > b[sortField] ? sortOrder : -sortOrder
     //     );
     //   }
-
     //   return filteredProducts;
     // },
   },
 
   actions: {
+    async getError() {
+      return this.error;
+    },
     async fetchProducts() {
       const { data } = await useFetch("https://dummyjson.com/products");
       if (data.value) {
@@ -42,9 +46,24 @@ export const dataStore = defineStore("products", {
         // console.log(data.value.products);
       } else {
         console.log(error);
+        this.error = true
       }
     },
 
+    async searchProducts(searchTerm) {
+      const { data } = await useFetch(
+        `https://dummyjson.com/products/search?q=${searchTerm}`
+      );
+      if (data.value) {
+        this.products = data.value.products;
+        this.searchTerm = searchTerm;
+        this.error = false;
+      }
+      if (data.value.total == 0) {
+        console.log('pusto')
+        this.error = true;
+      } 
+    },
     // async fetchCategories() {
     //   const response = await axios.get(
     //     "https://dummyjson.com/products/categories"
@@ -76,4 +95,7 @@ export const dataStore = defineStore("products", {
     //   }
     // },
   },
+  // return: {
+  //   fetchProducts(),
+  // }
 });
